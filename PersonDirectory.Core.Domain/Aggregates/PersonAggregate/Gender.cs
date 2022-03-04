d361e21db8d5ae8f114238ddc52a73fc900cb378
Giga <prodigygiga@gmail.com>
2022-03-05 01:21:23 +0400

    added gender enum

---
+++ PersonDirectory.Core.Domain/Aggregates/PersonAggregate/Gender.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Core.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonDirectory.Core.Domain.Aggregates.PersonAggregate
+{
+    public class Gender : Enumeration
+    {
+        public static Gender Male = new(1, nameof(Male));
+        public static Gender Female = new(2, nameof(Female));
+
+        public Gender(int id, string name)
+            : base(id, name)
+        {
+        }
+    }
+}
--- PersonDirectory.Core.Domain/Aggregates/PersonAggregate/Person.cs
+++ PersonDirectory.Core.Domain/Aggregates/PersonAggregate/Person.cs
@@ -15,11 +15,28 @@ namespace PersonDirectory.Core.Domain.Aggregates.PersonAggregate
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
         public string PrivateNumber { get; private set; }
+        public Gender Gender { get; private set; }
         public DateTime BirthDate { get; private set; }
         public int CityId { get; private set; }
         public City City { get; private set; }
         public string PicturePath { get; private set; }
         public IReadOnlyCollection<PhoneNumber> PhoneNumbers => phoneNumbers;
         public IReadOnlyCollection<PersonRelation> Relations => relations;
+        protected Person()
+        {
+            phoneNumbers = new HashSet<PhoneNumber>();
+            relations = new HashSet<PersonRelation>();
+        }
+
+        public Person(string firstName, string lastName, string privateNumber,Gender gender, DateTime birthDate, int cityId,string picturePath)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            PrivateNumber = privateNumber;
+            Gender = gender;
+            BirthDate = birthDate;
+            CityId = cityId;
+            PicturePath = picturePath;
+        }
     }
 }
